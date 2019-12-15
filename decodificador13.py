4f864bce71ba17d21327647ff54b799077fce027
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-15 10:22:41 -0500

    se ingreso 13 ejercicio

---
+++ decodificador13.py
@@ -0,0 +1,16 @@
+#INPUT
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    print(letra)
+#decodificador
+    if(letra=="v"):
+        print("viva")
+    if(letra=="e"):
+        print("el")
+    if(letra=="p"):
+        print("Peru")
+    if(letra=="h"):
+        print("hermoso")
+
+#fin_for
