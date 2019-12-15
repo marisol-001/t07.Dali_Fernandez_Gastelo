68cefcf89b858dc5393734c94cad4cb6d1b49759
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-15 10:32:27 -0500

    se ingreso 15 ejercicio

---
+++ decodificador15.py
@@ -0,0 +1,20 @@
+#INPUT
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    print(letra)
+#decodificador
+    if(letra=="a"):
+        print("porque")
+    if(letra=="e"):
+        print("la")
+    if(letra=="i"):
+        print("musica")
+    if(letra=="o"):
+        print("es el")
+    if(letra=="u"):
+        print("sentir de ")
+    if(letra=="x"):
+        print("nuestro pueblo ")
+
+#fin_for
