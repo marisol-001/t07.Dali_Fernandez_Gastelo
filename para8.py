f7411a5250a9b416db700fb819ffc90cd9c06c44
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-14 19:28:23 -0500

    se ingreso 8to ejercicio

---
+++ para8.py
@@ -0,0 +1,5 @@
+#INPUT
+#Escribir un programa que pida al usuario una palabra y la muestre 10 veces por pantalla.
+palabra = input("Introduce una palabra: ")
+for i in range(10):
+    print(palabra)
