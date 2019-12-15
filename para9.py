ff994ce2a71bedbb46e4656b45000c57fe1df47d
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-14 19:33:47 -0500

    se ingreso 9no ejercicio

---
+++ para9.py
@@ -0,0 +1,10 @@
+#INPUT
+n=0
+n = int(input("Introduce un número entero positivo mayor que 2: "))
+for i in range(2, n):
+    if (n % i) == 0:
+        break
+if (i + 1)  == n:
+    print(str(n) + " es primo")
+else:
+    print(str(n) + " no es primo")
