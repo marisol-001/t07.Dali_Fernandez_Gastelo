8d6caee75852975eda104395cbf2262989310461
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-15 10:24:52 -0500

    se ingreso 14 ejercicio

---
+++ decodificador14.py
@@ -0,0 +1,16 @@
+#INPUT
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    print(letra)
+#decodificador
+    if(letra=="e"):
+        print("somos")
+    if(letra=="f"):
+        print("una")
+    if(letra=="g"):
+        print("nacion")
+    if(letra=="h"):
+        print("unida")
+
+#fin_for
