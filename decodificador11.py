dd8a6f9d8284d26632bc88150bc1a12290a5c9c4
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-15 09:46:22 -0500

    se ingreso 11 ejercicio

---
+++ decodificador11.py
@@ -0,0 +1,13 @@
+#INPUT
+msg="abc"
+for letra in msg:
+    print(letra)
+#decodificador
+    if(letra=="a"):
+        print("te quiero")
+    if(letra=="b"):
+        print("para")
+    if(letra=="c"):
+        print("siempre")
+
+#fin_for
