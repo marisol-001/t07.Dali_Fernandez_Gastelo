a4b1ac50216817a77233bb43bd4e9ea7d3d9d216
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-15 10:13:50 -0500

    se ingreso 12 ejercicio

---
+++ decodificador12.py
@@ -0,0 +1,16 @@
+#INPUT
+import os
+msg=os.sys.argv[1]
+for letra in msg:
+    print(letra)
+#decodificador
+    if(letra=="x"):
+        print("aprueba")
+    if(letra=="y"):
+        print("tus")
+    if(letra=="z"):
+        print("cursos")
+    if(letra=="w"):
+        print("por favor")
+
+#fin_for
