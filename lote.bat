38727f037217a3a42d66bda777632cfd8a91ea5c
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-15 23:51:17 -0500

    se ingreso lote.bat

---
+++ lote.bat
@@ -0,0 +1,19 @@
+python decodificador11.py "a b c"
+python decodificador11.py "a v b"
+python decodificador11.py "n m e"
+
+python decodificador12.py "n m k l"
+python decodificador12.py "v g h y"
+python decodificador12.py "x y z w"
+
+python decodificador13.py "v e p h"
+python decodificador13.py "s m j k"
+python decodificador13.py "p o e r"
+
+python decodificador14.py "a b c d"
+python decodificador14.py "e f g h"
+python decodificador14.py "b k l m"
+
+python decodificador15.py "a b c d e f"
+python decodificador15.py "a e i o u x"
+python decodificador15.py "m n j l o p"
